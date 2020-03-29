0583244b5739e91ed4b02174d8f1b5052991bc1b
chokio0505 <chokio1118@gmail.com>
2020-03-29 17:20:40 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = (props) => {
   const [state, setState] = useState(props);
-  const {name, price} = state;
+  const { name, price } = state;
+
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate.');
+  });
+
+  useEffect(() => {
+    console.log("This is like componentDidMount");
+  }, []);
+
+  useEffect(() => {
+    console.log("This callback is for name only");
+  }, [name]);
+
 
   return (
     <>
       <p>現在の{state.name}は、{state.price}円です。</p>
-      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
-      <button onClick={() => setState(props)}>Reset</button> 
-      <input value={name} onChange={e => setState({...state, name: e.target.value})} />
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({ ...state, name: e.target.value })} />
     </>
   );
 }
