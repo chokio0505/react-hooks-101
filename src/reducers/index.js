a5f356b78a37db946f7f4f066f1889cd39a9399c
chokio0505 <chokio1118@gmail.com>
2020-03-31 07:40:18 +0900

    implement event creattion

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 
 const events = (state = [], action) => {
+    debugger
     switch(action.type) {
-        case 'CERATE_EVENT':
+        case 'CREATE_EVENT':
             const event = {title: action.title, body: action.body}
             const length = state.length;
             let id = (length === 0) ? 1 : state[length - 1].id + 1;
